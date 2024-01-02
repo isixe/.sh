793d734383b2cb068384d90bca5d15e8feb4deec
isixe <isixe@outlook.com>
2024-01-02 19:18:39 +0800

    Add files via upload

--- docker/docker(debian).sh
+++ docker/docker(debian).sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc
+do
+   sudo apt-get remove $pkg
+done
 
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/dockersudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+docker version
+docker-compose version
